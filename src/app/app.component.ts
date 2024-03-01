435fa3ccf76657a9d0598c40b847b0b7be2ce575
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2024-03-01 17:45:29 +0300

    adds routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { NgbCollapse } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, NgbCollapse, RouterLink],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pusers';
+    isNavCollapsed = true;
+
+    constructor(public router: Router) {}
 }
