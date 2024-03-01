4a41da895cb79d0387075a055c031253a3b38cc3
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2024-03-01 17:39:57 +0300

    adds users list

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from "../../services/users.service";
+import { User } from "../../services/users.model";
+import { environment } from "../../../environments/environment";
+import { NgOptimizedImage } from "@angular/common";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
     selector: 'app-user-list',
     standalone: true,
-    imports: [],
+    imports: [
+        NgOptimizedImage,
+    ],
     templateUrl: './user-list.component.html',
     styleUrl: './user-list.component.scss'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+    loadingDetails = { isFetching: false, isSaving: false };
+    avatarUrl = environment.avatar;
+    users: Array<User> = [];
 
+    constructor(private service: UsersService, private toaster: ToastrService) {}
+
+    ngOnInit(): void {
+        this.fetchUsers({});
+    }
+
+    fetchUsers(filter:  { [key: string]: string; }) {
+        this.users = [];
+        this.loadingDetails.isFetching = true;
+        this.service.getAllUsers(filter).subscribe(
+            (data) => {
+                if (data.success) {
+                    this.users = data.data;
+                } else {
+                    this.toaster.error(`Could not load users: ${data.message}`);
+                }
+                this.loadingDetails.isFetching = false;
+            }
+        );
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    url: "https://jsonplaceholder.typicode.com/users/"
+    url: "https://jsonplaceholder.typicode.com/users/",
+    avatar: "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg"
 };
